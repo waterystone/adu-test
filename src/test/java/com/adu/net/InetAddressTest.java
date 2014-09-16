93ae1c877a812657d23f96c2d8ccdbc6211e059b
yunjiedu <yunjiedu@sohu-inc.com>
2014-09-16 19:23:45 +0800

    test

---
+++ src/test/java/com/adu/net/InetAddressTest.java
@@ -0,0 +1,19 @@
+package com.adu.net;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+public class InetAddressTest {
+	private Log logger = LogFactory.getLog(this.getClass());
+
+	@Test
+	public void test() throws UnknownHostException {
+		String ret = InetAddress.getLocalHost().getHostAddress();
+		logger.debug("ret=" + ret);
+	}
+
+}
