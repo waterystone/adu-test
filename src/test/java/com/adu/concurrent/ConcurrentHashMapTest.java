fd6f21df0195f156ea7bf41a35844162319e47b8
yunjiedu <yunjiedu@sohu-inc.com>
2014-12-11 20:05:46 +0800

    test

--- src/test/java/com/adu/concurrent/ConcurrentHashMapTest.java
+++ src/test/java/com/adu/concurrent/ConcurrentHashMapTest.java
@@ -1,5 +1,7 @@
 package com.adu.concurrent;
 
+import java.util.Iterator;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.commons.logging.Log;
@@ -13,6 +15,18 @@ public class ConcurrentHashMapTest {
 	public void test() {
 		ConcurrentHashMap<Integer, String> map = new ConcurrentHashMap<Integer, String>();
 		map.put(1, "a");
-		logger.debug(map);
+		map.put(2, "b");
+		logger.debug("map1=" + map);
+
+		Iterator<Map.Entry<Integer, String>> iter = map.entrySet().iterator();
+		while (iter.hasNext()) {
+			Map.Entry<Integer, String> entry = iter.next();
+			if (entry.getKey() < 10) {
+				iter.remove();
+			}
+		}
+		logger.debug("map2=" + map);
+		map.put(3, "b");
+		logger.debug("map3=" + map);
 	}
 }
