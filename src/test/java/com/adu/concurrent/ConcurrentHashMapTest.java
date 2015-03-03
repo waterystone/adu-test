0719b374dd2ded959004fea48703555a0e366974
yunjiedu <yunjiedu@sohu-inc.com>
2015-03-03 17:16:58 +0800

    ADD:Map线程并发

--- src/test/java/com/adu/concurrent/ConcurrentHashMapTest.java
+++ src/test/java/com/adu/concurrent/ConcurrentHashMapTest.java
@@ -1,6 +1,11 @@
 package com.adu.concurrent;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -28,5 +33,29 @@ public class ConcurrentHashMapTest {
 		logger.debug("map2=" + map);
 		map.put(3, "b");
 		logger.debug("map3=" + map);
+	}
+
+	@Test
+	public void test1() throws InterruptedException {
+		final Map<Integer, String> map = new ConcurrentHashMap<Integer, String>();
+
+		for (int i = 0; i < 1000; i++) {
+			new Thread() {
+				public void run() {
+					for (int i = 0; i < 100; i++) {
+						map.put(i, i + "");
+					}
+				};
+
+			}.start();
+		}
+		for (Map.Entry<Integer, String> entry : map.entrySet()) {
+			logger.debug(entry);
+		}
+		Thread.sleep(10000);
+		List<Integer> keyList = new ArrayList<Integer>(map.keySet());
+		Collections.sort(keyList);
+		logger.debug("size=" + map.size() + ",keys=" + keyList);
+
 	}
 }
