6e3c9ab92afc281d19cd52ce06b74890e50394a9
yunjiedu <yunjiedu@sohu-inc.com>
2015-05-14 11:25:37 +0800

    ADD:JMMReorderTest

---
+++ src/test/java/com/adu/lang/CharTest.java
@@ -0,0 +1,15 @@
+package com.adu.lang;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+public class CharTest {
+	private final Log logger = LogFactory.getLog(this.getClass());
+
+	@Test
+	public void test() {
+		char res = '中';
+		logger.debug("res=" + res);
+	}
+}
