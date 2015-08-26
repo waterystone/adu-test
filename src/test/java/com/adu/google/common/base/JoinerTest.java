7a7177ac25adbe8f04e57664d661766497c07070
yunjie.du <yunjie.du@qunar.com>
2015-08-26 20:12:12 +0800

    MOD:TEST

--- src/test/java/com/adu/google/common/base/JoinerTest.java
+++ src/test/java/com/adu/google/common/base/JoinerTest.java
@@ -1,15 +1,15 @@
 package com.adu.google.common.base;
 
-import com.adu.lang.StringBufferTest;
-import com.google.common.base.Joiner;
-import com.google.common.collect.Maps;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Maps;
 
 public class JoinerTest {
     private final Log logger = LogFactory.getLog(this.getClass());
@@ -23,10 +23,11 @@ public class JoinerTest {
 
     @Test
     public void withKeyValueSeparator() {
-        Map<String, Integer> parameters = Maps.newHashMap();
-        parameters.put("a", 1);
-        parameters.put("b", 2);
-        parameters.put("c", 3);
+        Map<String, Integer> parameters = Maps.newTreeMap();
+        parameters.put("ad", 1);
+        parameters.put("aa", 4);
+        parameters.put("ac", 2);
+        parameters.put("ab", 3);
         String res = Joiner.on("&").withKeyValueSeparator("=").join(parameters);
         logger.debug(res);
     }
---
+++ src/test/java/com/adu/org/apache/commons/lang3/StringUtilsTest.java
@@ -0,0 +1,17 @@
+package com.adu.org.apache.commons.lang3;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+public class StringUtilsTest {
+	private final Log logger = LogFactory.getLog(this.getClass());
+
+	@Test
+	public void test() {
+		String str = "[3-2-5, 3-2-6]";
+		String res = StringUtils.substring(str, 1, -1);
+		logger.debug("res=" + res);
+	}
+}
