3706168deaa93b78e03733e2744f529280c2db47
yunjie.du <yunjie.du@qunar.com>
2016-03-18 16:27:23 +0800

    MOD:test

--- src/test/java/com/adu/HelloTest.java
+++ src/test/java/com/adu/HelloTest.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class HelloTest {
-	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@Test
-	public void test() {
-		String res = "Hello,World!";
-		logger.debug("res={}", res);
-	}
+public class HelloTest {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Test
+    public void test() {
+        String res = "Hello,World!";
+        logger.debug("res={}", res);
+    }
 
 }
