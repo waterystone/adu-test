01990478876e3ac124941d8100f1775359e71fa1
yunjie.du <yunjie.du@qunar.com>
2016-07-26 17:33:25 +0800

    ADD:权重随机算法

---
+++ src/main/java/com/adu/utils/WeightRandom.java
@@ -0,0 +1,50 @@
+package com.adu.utils;
+
+import com.google.common.base.Preconditions;
+import org.apache.commons.math3.util.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * 权重随机算法的实现。
+ * 如有4个元素A、B、C、D，权重分别为1、2、3、4。
+ * <p>
+ * 总体思路:
+ * 累加每个元素的权重A(1)-B(3)-C(6)-D(10)，然后随机出一个(0,10)之间的随机数，落在哪个区间,则该区间之后的元素即为按权重命中的元素。
+ * <p>
+ * 实现算法：
+ * 利用TreeMap，则构造出的一个树为:
+ * B(3)
+ * /    \
+ * /       \
+ * A(1)      D(10)
+ * /
+ * /
+ * C(6)
+ *
+ * @author yunjie.du
+ * @date 2016/7/26 15:06
+ */
+public class WeightRandom<K extends Number, V> {
+    private TreeMap<Double, V> weightMap = new TreeMap<Double, V>();
+    private static final Logger logger = LoggerFactory.getLogger(WeightRandom.class);
+
+    public WeightRandom(List<Pair<V, K>> list) {
+        Preconditions.checkNotNull(list, "list can NOT be null!");
+        for (Pair<V, K> pair : list) {
+            double lastWeight = this.weightMap.size() == 0 ? 0 : this.weightMap.lastKey().doubleValue();//统一转为double
+            this.weightMap.put(pair.getValue().doubleValue() + lastWeight, pair.getKey());//权重累加
+        }
+    }
+
+    public V random() {
+        double randomWeight = this.weightMap.lastKey() * Math.random();
+        SortedMap<Double, V> tailMap = this.weightMap.tailMap(randomWeight, false);
+        return this.weightMap.get(tailMap.firstKey());
+    }
+
+}
---
+++ src/test/java/com/adu/utils/WeightRandomTest.java
@@ -0,0 +1,45 @@
+package com.adu.utils;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.commons.math3.util.Pair;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+
+public class WeightRandomTest {
+    List<Pair<String, Integer>> list;
+    private WeightRandom<Integer, String> random;
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Test
+    public void random() {
+        Map<String, Integer> countMap = Maps.newHashMap();
+        for (int i = 0; i < 100000000; i++) {
+            String randomValue = random.random();
+            countMap.put(randomValue, countMap.getOrDefault(randomValue, 0) + 1);
+        }
+
+        for (Pair<String, Integer> pair : list) {
+            logger.debug("{}:{}", pair.getKey(), countMap.get(pair.getKey()));
+        }
+    }
+
+    @Before
+    public void init() {
+        list = Lists.newArrayList();
+        list.add(new Pair("A", 1));
+        list.add(new Pair("B", 2));
+        list.add(new Pair("C", 3));
+        list.add(new Pair("D", 4));
+
+        this.random = new WeightRandom(list);
+    }
+
+}
