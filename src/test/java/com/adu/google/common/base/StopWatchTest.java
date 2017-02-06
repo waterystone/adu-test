57198da3fa47589b864d093c868e9fc30ba96ef0
yunjie.du <yunjie.du@qunar.com>
2017-02-06 17:14:01 +0800

    ADD:StopWatch Test

---
+++ src/test/java/com/adu/google/common/base/StopWatchTest.java
@@ -0,0 +1,26 @@
+package com.adu.google.common.base;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import com.adu.BaseTest;
+import com.google.common.base.Stopwatch;
+
+/**
+ * @author yunjie.du
+ * @date 2017/2/6 15:45
+ */
+public class StopWatchTest extends BaseTest {
+
+    @Test
+    public void test() throws InterruptedException {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+
+        Thread.sleep(2000);
+
+        stopwatch.stop();
+        long res = stopwatch.elapsed(TimeUnit.MILLISECONDS);
+        logRes(res);
+    }
+}
