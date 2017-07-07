24ce4bc19b5791f7677c6628267d42aa185c41f0
yunjie.du <yunjie.du@qunar.com>
2017-07-07 19:13:04 +0800

    ADD:BloomFilterTest

--- src/test/java/com/adu/jdk/lang/ExceptionTest.java
+++ src/test/java/com/adu/jdk/lang/ExceptionTest.java
@@ -1,15 +1,33 @@
 package com.adu.jdk.lang;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.junit.Test;
 
-public class ExceptionTest {
-	private final Log logger = LogFactory.getLog(this.getClass());
+import com.adu.BaseTest;
 
-	@Test
-	public void test() {
-		Exception res = new Exception();
-		logger.debug("res=" + res);
-	}
+public class ExceptionTest extends BaseTest {
+
+    @Test
+    public void test() {
+        Exception res = new Exception();
+        logRes(res);
+    }
+
+    @Test
+    public void test1() {
+        int res = fun();
+        logRes(res);
+    }
+
+    public int fun() {
+        try {
+            int res = 1 / 0;
+            return res;
+        } catch (Exception e) {
+            logger.info("[ERROR-exception]", e);
+            return 1;
+        } finally {
+        	logger.info("finally");
+            return 2;
+        }
+    }
 }
