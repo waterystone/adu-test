7687489d63c6dda173747ed367f1248d293b0de4
yunjie.du <yunjie.du@qunar.com>
2017-10-16 16:01:13 +0800

    MOD:test

--- src/test/java/com/adu/HelloTest.java
+++ src/test/java/com/adu/HelloTest.java
@@ -2,16 +2,12 @@ package com.adu;
 
 import org.junit.Test;
 
-import java.util.Date;
-
 public class HelloTest extends BaseTest {
 
     @Test
     public void test() {
         String res = "Hello,World!";
-
-        long time = new Date().getTime();
-        logRes(time);
+        logRes(res);
     }
 
 }
