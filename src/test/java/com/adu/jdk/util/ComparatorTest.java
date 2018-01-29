e162c0d8f2d0b65ee14dfbb304809b61fa97b4c6
duyunjie <duyunjie@kuaishou.com>
2018-01-29 11:34:30 +0800

    ADD:sort

--- src/test/java/com/adu/jdk/util/ComparatorTest.java
+++ src/test/java/com/adu/jdk/util/ComparatorTest.java
@@ -11,11 +11,17 @@ import com.adu.BaseTest;
 public class ComparatorTest extends BaseTest {
 
     Comparator<String> comparator = (String s1, String s2) -> s1.compareTo(s2);
-    List<String> list = Arrays.asList("a", "c", "b");
+    List<String> list = Arrays.asList("ab", "c", "def");
 
     @Test
     public void sort() {
         list.sort(comparator);
+        print(list);
+    }
+
+    @Test
+    public void comparingInt() {
+        list.sort(Comparator.comparingInt(e -> e.length()));
         print(list);
     }
 }
