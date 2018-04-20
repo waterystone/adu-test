c1473e790f69cd8286475efb0bb90bc190071e19
duyunjie <duyunjie@kuaishou.com>
2018-04-20 15:37:23 +0800

    ADD:UninterruptiblesTest

--- src/test/java/com/adu/jdk/net/URLTest.java
+++ src/test/java/com/adu/jdk/net/URLTest.java
@@ -1,13 +1,14 @@
 package com.adu.jdk.net;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class URLTest {
+
     private URL url = this.getClass().getResource("/");
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -25,14 +26,14 @@ public class URLTest {
 
     @Test
     public void getPort() throws MalformedURLException {
-        url=new URL("http://www.baidu.com:8087?keywords=me&from=test");
+        url = new URL("http://www.baidu.com:8087?keywords=me&from=test");
         int res = url.getPort();
         logger.debug("res={}", res);
     }
 
     @Test
     public void getQuery() throws MalformedURLException {
-        url=new URL("http://www.baidu.com?keywords=me&from=test");
+        url = new URL("http://www.baidu.com?keywords=me&from=test");
         String res = url.getQuery();
         logger.debug("res={}", res);
     }
