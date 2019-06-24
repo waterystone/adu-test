05982d4b8c991c115335d0efce2e00cd71fb68cf
duyunjie <duyunjie@kuaishou.com>
2019-06-24 10:15:50 +0800

    ADD：nanoTimeTest

--- src/test/java/com/adu/jdk/lang/SystemTest.java
+++ src/test/java/com/adu/jdk/lang/SystemTest.java
@@ -1,8 +1,11 @@
 package com.adu.jdk.lang;
 
+import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 
 import com.adu.BaseTest;
+import com.google.common.util.concurrent.Uninterruptibles;
 
 public class SystemTest extends BaseTest {
 
@@ -11,5 +14,14 @@ public class SystemTest extends BaseTest {
         long ms = System.currentTimeMillis();
         long ns = System.nanoTime();
         logger.debug("ms={},ns={}", ms, ns);
+    }
+
+    @Test
+    public void nanoTime1() {
+        for (int i = 0; i < 10; i++) {
+            long ns = System.nanoTime();
+            logger.debug("i={},ns={}", i, ns);
+            Uninterruptibles.sleepUninterruptibly(2, TimeUnit.SECONDS);
+        }
     }
 }
