64b29cde82da8ba494875254cb39d27b4485cb81
duyunjie <duyunjie@zhuanzhuan.com>
2019-10-22 20:14:08 +0800

    MOD:单个类的logger配置

--- src/test/java/com/adu/HelloTest.java
+++ src/test/java/com/adu/HelloTest.java
@@ -7,7 +7,7 @@ public class HelloTest extends BaseTest {
     @Test
     public void test() {
         String res = "Hello,World!";
-
+        logger.debug("op=end_test,res={}", res);
         logRes(res);
     }
 
