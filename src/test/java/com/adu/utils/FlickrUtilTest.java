05698fd02fea4262201fa901503fbd33555d1fbb
duyunjie <duyunjie@zhuanzhuan.com>
2019-10-28 19:41:06 +0800

    MOD:多线程方式

--- src/test/java/com/adu/utils/FlickrUtilTest.java
+++ src/test/java/com/adu/utils/FlickrUtilTest.java
@@ -2,14 +2,19 @@ package com.adu.utils;
 
 import com.adu.BaseTest;
 import com.google.common.collect.Maps;
+import com.google.common.util.concurrent.Uninterruptibles;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author duyunjie
@@ -38,37 +43,67 @@ public class FlickrUtilTest extends BaseTest {
 
     @Test
     public void searchPhotoInfos() throws Exception {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        HttpClientUtil.init(20, 5);
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        SearchPhotoInfoTask searchPhotoInfoTask1 = new SearchPhotoInfoTask(LocalDate.parse("2012-01-01"), LocalDate.parse("2013-12-31"));
+        SearchPhotoInfoTask searchPhotoInfoTask2 = new SearchPhotoInfoTask(LocalDate.parse("2014-01-01"), LocalDate.parse("2015-12-31"));
+        SearchPhotoInfoTask searchPhotoInfoTask3 = new SearchPhotoInfoTask(LocalDate.parse("2016-01-01"), LocalDate.parse("2017-12-31"));
+        SearchPhotoInfoTask searchPhotoInfoTask4 = new SearchPhotoInfoTask(LocalDate.parse("2018-01-01"), LocalDate.parse("2019-10-31"));
 
-        Map<String, String> params = Maps.newHashMap();
-        params.put("bbox", "116.06,39.80,116.65,40.16");
-        params.put("geo_context", "2");
-        params.put("content_type", "1");
-        params.put("per_page", "200");
+        executorService.submit(searchPhotoInfoTask1);
+        executorService.submit(searchPhotoInfoTask2);
+        executorService.submit(searchPhotoInfoTask3);
+        executorService.submit(searchPhotoInfoTask4);
 
-        LocalDate startDate = LocalDate.parse("2011-08-01");
-        LocalDate maxDate = LocalDate.parse("2019-10-31");
-        while (true) {
-            LocalDate endDate = startDate.plusMonths(1);
-            params.put("min_taken_date", formatter.format(startDate));
-            params.put("max_taken_date", formatter.format(endDate));
-            String res = FlickrUtil.searchPhotoInfos(params, 1, 30);
-            if (StringUtils.isEmpty(res)) {
-                logger.info("[empty_searchPhotoInfos]startDate={},endDate={}", startDate, endDate);
-                continue;
-            }
+        Uninterruptibles.sleepUninterruptibly(3, TimeUnit.DAYS);
+    }
 
-            String fileName = String.format("D:\\data\\flickr\\%s.txt", formatter.format(startDate));
-            //String fileName = String.format("/Users/yunjie.du/data/flickr/2/data/%s.txt", formatter.format(startDate));
-            Files.write(Paths.get(fileName), res.getBytes("UTF-8"));
-            logger.info("[success_write_file]startDate={}", startDate);
+    class SearchPhotoInfoTask implements Runnable {
+        private LocalDate minDate;
+        private LocalDate maxDate;
 
-            startDate = endDate;
-            if (startDate.isAfter(maxDate)) {
-                break;
+        public SearchPhotoInfoTask(LocalDate minDate, LocalDate maxDate) {
+            this.minDate = minDate;
+            this.maxDate = maxDate;
+        }
+
+        @Override
+        public void run() {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            Thread.currentThread().setName(String.format("search-%s", formatter.format(minDate)));
+            LocalDate startDate = minDate;
+
+            Map<String, String> params = Maps.newHashMap();
+            params.put("bbox", "116.06,39.80,116.65,40.16");
+            params.put("geo_context", "2");
+            params.put("content_type", "1");
+            params.put("per_page", "200");
+
+            while (true) {
+                try {
+                    LocalDate endDate = startDate.plusMonths(1);
+                    params.put("min_taken_date", formatter.format(startDate));
+                    params.put("max_taken_date", formatter.format(endDate));
+                    String res = FlickrUtil.searchPhotoInfos(params, 1, 30);
+                    if (StringUtils.isEmpty(res)) {
+                        logger.info("[empty_searchPhotoInfos]startDate={},endDate={}", startDate, endDate);
+                        continue;
+                    }
+
+                    String fileName = String.format("D:\\data\\flickr\\%s.txt", formatter.format(startDate));
+                    //String fileName = String.format("/Users/yunjie.du/data/flickr/2/data/%s.txt", formatter.format(startDate));
+                    Files.write(Paths.get(fileName), res.getBytes("UTF-8"));
+                    logger.info("[success_write_file]startDate={}", startDate);
+
+                    startDate = endDate;
+                    if (startDate.isAfter(maxDate)) {
+                        break;
+                    }
+                } catch (IOException e) {
+                    logger.error("[ERROR_run]startDate={},endDate={}", startDate, e);
+                }
             }
         }
-        //logRes(res);
     }
 
 
